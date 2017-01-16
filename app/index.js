49472b726275dc2181699553ea07fc3e4088711b
Robert <viniolli@gmail.com>
2017-01-16 23:55:37 +0100

    course 1 - video 3

--- app/index.js
+++ app/index.js
@@ -1,15 +1,52 @@
+var USER_DATE = {
+	name: 'Tyler McGinnis',
+	username: 'tylermcginnis',
+	image: 'https://avatars0.githubusercontents.com/u/2933430?v=3&s=460'
+}
+
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+	render: function(){
+		return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+	}
+});
+
+var ProfileLink = React.createClass({
 	render: function() {
 		return (
-			<div> Hello react js </div>
+			<div>
+				<a href={'https://www.github.com/' + this.props.username}>
+					{this.props.username}
+				</a>
+			</div>
+		)
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div>{this.props.name}</div>
+		)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
 		)
 	}
 });
 
 ReactDOM.render(
-	<HelloWorld />,
+	<Avatar user={USER_DATE} />,
 	document.getElementById('app')
 )
