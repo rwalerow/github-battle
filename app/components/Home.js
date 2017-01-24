f1ed09c76630e70731e0775c1bee8e0dd81de0a5
Robert <viniolli@gmail.com>
2017-01-24 23:02:14 +0100

    courese 1 video 13

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,14 +2,12 @@ var React = require('react');
 var transparentBG = require('../styles').transparentBG;
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
+var MainContainer = require('./MainContainer');
 
 var Home = React.createClass({
 	render: function() {
 		return (
-			<div
-				className="jumbotron col-sm-12 text-center"
-				style={transparentBG}>
-				
+			<MainContainer>
 				<h1>Github Battle</h1>
 				<p className="lead">Some fancy motto</p>
 				<Link to='/playerOne'>
@@ -17,7 +15,7 @@ var Home = React.createClass({
 						Get Started
 					</button>
 				</Link>
-			</div>
+			</MainContainer>
 		)
 	}
 });
